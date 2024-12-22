5a44083d7102858526ccc521e2be84475e1837aa
brucekang <radeonk@foxmail.com>
2024-12-22 21:41:40 +0800

    init

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,25 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
-};
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended'
+  ],
+  plugins: ['react', 'react-native'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2020, // 支持最新的 ECMAScript 版本
+    sourceType: 'module'
+  },
+  settings: {
+    react: {
+      version: 'detect' // 自动检测所安装的 React 版本
+    }
+  },
+  rules: {
+    // 可以在这里自定义规则
+    'prettier/prettier': 0,
+    'no-undef': 0
+  }
+}
